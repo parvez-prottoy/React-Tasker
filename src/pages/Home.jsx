e37c0a8bb73208a4820a5e2efb05ba5e51ed91a8
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-11-10 17:48:24 +0600

    added footer components design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import TaskBoard from "../components/TaskBoard";
 
@@ -6,6 +7,7 @@ const Home = () => {
     <>
       <Header />
       <TaskBoard />
+      <Footer />
     </>
   );
 };
