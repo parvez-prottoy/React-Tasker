068831f2420d6e603494d5e6b2cef6d39e3ac0c6
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-11-10 15:05:50 +0600

    added header component design

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header>
+      <nav className="py-4 md:py-6 fixed top-0 w-full z-50 border-b border-b-[rgba(206,206,206,0.12)]">
+        <div className="container flex items-center justify-between gap-x-6">
+          <h4 className="cursor-pointer text-3xl font-extrabold text-[#F5BF42]">
+            Tasker
+          </h4>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
